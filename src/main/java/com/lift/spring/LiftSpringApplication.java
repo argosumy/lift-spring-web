cadedb24e80cfb6b6062d5556d6bfe06e6ba3a24
LozinskyiValerii <argosumy@gmail.com>
2021-05-21 15:16:05 +0300

    Initial commit

---
+++ src/main/java/com/lift/spring/LiftSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lift.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiftSpringApplication.class, args);
+    }
+
+}
